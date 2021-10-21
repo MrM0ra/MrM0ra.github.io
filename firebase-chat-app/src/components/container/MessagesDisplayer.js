c76dd07afd92307bfdd08530984a37be6df30243
unknown <themonstermora@gmail.com>
2021-10-20 23:54:14 -0500

    options on messages added, functionalities still left to develop

--- firebase-chat-app/src/components/container/MessagesDisplayer.js
+++ firebase-chat-app/src/components/container/MessagesDisplayer.js
@@ -4,6 +4,13 @@ import { styles } from "../../styles/styles";
 export const MessageDisplayer = (props) => {
     return(
         <div style={styles.messageBubble}>
+            <select>
+                <option>...</option>
+                <option>Responder</option>
+                <option>Eliminar</option>
+                <option>Editar</option>
+            </select>
+            <br></br>
             <span><b>{props.msg.owner}: </b></span>
             {props.msg.msg}
         </div>
